cead37be157e6c85f5acf57a5f94f68bba2b0e47
Jörgen Thelin <thelin.jorgen@gmail.com>
2024-10-29 21:39:40 +0100

    cors

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow these origins
+    allow_credentials=True,  # Allow cookies and headers
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 ROOT_PATH = "/api"
 
