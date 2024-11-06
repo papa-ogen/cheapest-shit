9ac2859a96ebea03f121ca43527e0955556018ba
Alexander Lindqvist <alexander@solitudedesign.se>
2024-11-06 11:01:32 +0100

    Add frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,34 @@
 import "./style.css";
 
 async function fetchData() {
-  const response = await fetch(
-    "http://localhost:8000/api/v1/search?query=hello"
-  );
+  const response = await fetch("http://localhost:8000/api/v1/search?query=hello");
   const data = await response.json();
   console.log(data);
+
+  // Rendera produkterna inuti productGrid-diven
+  const productGridHTML = data.products.map(product => product_cart(product)).join('');
+
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <main>
+      <div class="productGrid">
+        ${productGridHTML}
+      </div><!--/productGrid-->
+    </main>
+  `;
 }
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <main>
-  <h1 class="title">Jugge och Alex</h1>
-  <img src="/morran_tobias.jpg" class="logo" alt="Jugge och Alex" />
-  </main>
-`;
+function product_cart(product) {
+  return `
+    <div class="productCard">
+      <img src="${product.image}">
+      <h2>${product.brand} ${product.name}</h2>
+      <p>Fr. ${product.price} kr på ${product.provider}</p>
+      <div>
+        <button>Till butik</button>
+      </div>
+    </div>
+  `;
+}
 
-// just making sure the backend works. Check browser console for the data
+// Anropa fetchData för att hämta och visa produkterna
 fetchData();
